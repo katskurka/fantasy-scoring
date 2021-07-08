654001b69433166a69ff846ae0c8c7912449cb40
katskurka <kati.skurka@gmail.com>
2021-07-07 22:21:31 -0400

    added function for qb stats calculation

--- scoring.js
+++ scoring.js
@@ -1,32 +1,53 @@
-const point = 1;
+/* const point = 1;
 let yardsPassing = 14.52*point;
 let passingTouchdown = ;
 let interception = point
 let rushing = ;
 let rushingTouchdown =;
-let rushingFumble = 
+let rushingFumble = */
 
-function checkPosition (player) { //make switch statements
-if (player.position === 'QB') {
+function qbScoring(qbStats) {
+  let passingYardScore = qbStats.passing.yards / 25
+  let passingTouchdownScore = qbStats.passing.touchdowns * 6
+  let interceptionScore = qbStats.passing.interceptions * -3
+  let rushingYardScore = qbStats.rushing.yards / 10
+  let rushingTouchdownScore = qbStats.rushing.touchdowns * 6
+  let rushingFumbleScore = qbStats.rushing.fumbles * -3
 
+  return passingYardScore + passingTouchdownScore + interceptionScore + rushingYardScore + rushingTouchdownScore + rushingFumbleScore
 }
-else if (player.position === 'RB') {
-  
+
+function rbScoring(rbStats) {
+  let passingYardScore = rbStats.passing.yards / 25
+  let passingTouchdownScore = rbStats.passing.touchdowns * 6
+  let interceptionScore = rbStats.passing.interceptions * -3
+  let rushingYardScore = rbStats.rushing.yards / 10
+  let rushingTouchdownScore = rbStats.rushing.touchdowns * 6
+  let rushingFumbleScore = rbStats.rushing.fumbles * -3
+
+  return passingYardScore + passingTouchdownScore + interceptionScore + rushingYardScore + rushingTouchdownScore + rushingFumbleScore
 }
-else if (player.position === 'WR') {
-  
-}
-else if (player.position === 'TE') {
-  
-}
-else return 0;
+
+function checkPosition(player) { // make switch statements
+  if (player.position === 'QB') {
+    return qbScoring(player.stats)
+  }
+  else if (player.position === 'RB') {
+    return rbScoring(player.stats)
+  }
+  else if (player.position === 'WR') {
+    return wrScoring(player.stats)
+  }
+  else if (player.position === 'TE') {
+    return teScoring(player.stats)
+  }
+  else return 0
 }
 function calculateScore(player) {
-  let score = 0;
-  checkPosition(player)
-  
+  let score = checkPosition(player)
+
   return score
-} 
+}
 
 
 module.exports = calculateScore
