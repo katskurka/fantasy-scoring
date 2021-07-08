b62b3ba99a244b83146eafacef382694cb8aed1f
katskurka <kati.skurka@gmail.com>
2021-07-07 21:37:00 -0400

    added skeleton of solution

---
+++ scoring.js
@@ -0,0 +1,32 @@
+const point = 1;
+let yardsPassing = 14.52*point;
+let passingTouchdown = ;
+let interception = point
+let rushing = ;
+let rushingTouchdown =;
+let rushingFumble = 
+
+function checkPosition (player) { //make switch statements
+if (player.position === 'QB') {
+
+}
+else if (player.position === 'RB') {
+  
+}
+else if (player.position === 'WR') {
+  
+}
+else if (player.position === 'TE') {
+  
+}
+else return 0;
+}
+function calculateScore(player) {
+  let score = 0;
+  checkPosition(player)
+  
+  return score
+} 
+
+
+module.exports = calculateScore
